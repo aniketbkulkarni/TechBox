20f300c4589883645c38cec6d789bc6ab3d3b059
cheningD <cdduker@gmail.com>
2015-11-10 11:00:56 -0500

    Navbar buttons change pages

--- js/Nav.react.js
+++ js/Nav.react.js
@@ -35,7 +35,27 @@ var Nav = React.createClass({
     console.log("Signup");
   },
   navBar: function() {
-    return (<span>Hello There</span>);
+    return (<span><nav className="navbar navbar-default" role="navigation-demo" id="demo-navbar">
+                <div className="container">
+                  <div className="navbar-header">
+                    <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#navigation-example-2">
+                      <span className="sr-only">Toggle navigation</span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                      <span className="icon-bar"></span>
+                    </button>
+                    <a className="navbar-brand" href="">TechBox</a>
+                  </div>
+                  <div className="collapse navbar-collapse" id="navigation-example-2">
+                    <ul className="nav navbar-nav navbar-right">
+                        <a href="#" className="btn btn-simple" onClick={this.clickFeat} >Featured</a>
+                        <a href="#" className="btn btn-simple" onClick={this.clickRecent}>Recently Added</a>  
+                        <a href="#" className="btn btn-simple" onClick={this.clickLogin} >Login</a>
+                        <a href="#" className="btn btn-simple" onClick={this.clickSignup} >Signup</a>
+                     </ul>
+                  </div>
+                </div>
+              </nav></span>);
   },
   render: function() {
       if (this.state.currentPage == "feat") { return (<div> 1 {this.navBar()}  <Featured/> </div>); }
