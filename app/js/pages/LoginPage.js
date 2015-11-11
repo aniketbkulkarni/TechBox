545a542186c15fd7a651968dc0b68d915d318e36
Aniket Kulkarni <aniketk@umich.edu>
2015-11-10 20:51:34 -0500

    parse login done, remove autogenerated js file

--- app/js/pages/LoginPage.js
+++ app/js/pages/LoginPage.js
@@ -1,29 +1,82 @@
 'use strict';
 
 import React            from 'react';
+import { render }       from 'react-dom'
+import Parse            from 'parse';
+import ParseReact       from 'parse-react';
 import {Link}           from 'react-router';
 import {Redirect}       from 'react-router';
 
 import DocumentTitle    from 'react-document-title';
+
+var ParseComponent = ParseReact.Component(React);
 
 
 const propTypes = {
   currentUser: React.PropTypes.object
 };
 
-class LoginPage extends React.Component {
+class LoginPage extends ParseComponent {
 
   constructor(props) {
     super(props);
     this.state = {
-      query: ''
+      error1: false,
+      email: '',
+      pass: ''
     };
+
+    this.handleEmail    = this.handleEmail.bind(this);
+    this.handlePass     = this.handlePass.bind(this);
+    this.handleSubmit   = this.handleSubmit.bind(this);
   }
 
-  handleQueryChange(evt) {
+
+  observe(props, state) {
+
+  }
+
+  handleEmail(event) {
     this.setState({
-      query: evt.target.value
+      email: event.target.value
     });
+  }
+
+  handlePass(event) {
+    this.setState({
+      pass: event.target.value
+    });
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    const email = this.state.email
+    const pass = this.state.pass
+
+    console.log(this.state.email);
+    console.log(this.state.pass);
+
+    Parse.User.logIn(email, pass, {
+      success: function(user) {
+        // Do stuff after successful login.
+
+
+        var token = user.getSessionToken();
+
+        Parse.User.become(token).then(function (user) {
+            // The current user is now set to user.
+            console.log("Success");
+          }, function (error) {
+            // The token could not be validated.
+          });
+
+      }, error: function(user, error) {
+        // The login failed. Check error to see why.
+        
+      }
+    });
+
   }
 
   render() {
@@ -36,23 +89,29 @@ class LoginPage extends React.Component {
                             <div className="row">
                                 <div className="col-md-4 col-md-offset-4 col-sm-6 col-sm-offset-3 col-xs-10 col-xs-offset-1 ">
                                     <div className="register-card">
-                                        <h3 className="title">Welcome</h3>
-                                        <form className="register-form">
-                                            <label>Email</label>
-                                            <input type="text" className="form-control" placeholder="Email" />
 
-                                            <label>Password</label>
-                                            <input type="password" className="form-control" placeholder="Password" />
-                                            <button className="btn btn-primary btn-block">Sign In</button>
-                                            <Link to="/register" className="btn btn-primary btn-block">Register</Link>
 
-                                        </form>
-                                        <div className="forgot">
+          <h3 className="title">Welcome</h3>
+          <form className="register-form">
 
-                                            <p><a href="#" target="_blank" className="btn btn-simple"><i className="fa fa-twitter"></i></a>
-                                            <a href="#" target="_blank" className="btn btn-simple"><i className="fa fa-facebook"></i></a></p>
-                                            
-                                        </div>
+            <label>Email</label>
+            <input value={this.state.email} onChange={this.handleEmail} type="text" className="form-control" placeholder="Email" />
+
+            <label>Password</label>
+            <input value={this.state.pass} onChange={this.handlePass} type="password" className="form-control" placeholder="Password" />
+
+            {this.state.error1 && (<p>Bad login information</p>)}
+
+            <button type="submit" onClick={this.handleSubmit} className="btn btn-primary btn-block">Sign In</button>
+            <Link to="/register" className="btn btn-primary btn-block">Register</Link>
+
+          </form>
+          <div className="forgot">
+
+              <p><a href="#" target="_blank" className="btn btn-simple"><i className="fa fa-twitter"></i></a>
+              <a href="#" target="_blank" className="btn btn-simple"><i className="fa fa-facebook"></i></a></p>
+              
+          </div>
                                     </div>
                                 </div>
                             </div>
